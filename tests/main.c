663aa12b88b7b1cd8dfe00d88a677e1cb9bbdea6
jxv <joevargas92@gmail.com>
2015-12-03 16:56:40 -0800

    Change nomenclature 'half -> inside'

--- tests/main.c
+++ tests/main.c
@@ -15,7 +15,7 @@ void eq_pos_test_aabb_vs_aabb_test(void **state) {
     assert_int_equal(pf_test_aabb_vs_aabb(&a, &a), true);
 }
 
-void half_up_pos_test_aabb_vs_aabb_test(void **state) {
+void inside_up_pos_test_aabb_vs_aabb_test(void **state) {
     const pf_aabb a = {
         .min = _v2f(-1.0, -1.0),
         .max = _v2f( 1.0,  1.0),
@@ -46,7 +46,7 @@ void border_up_pos_test_aabb_vs_aabb_test(void **state) {
 int main(void) {
     const struct CMUnitTest aabb_tests[] = {
         cmocka_unit_test(eq_pos_test_aabb_vs_aabb_test),
-        cmocka_unit_test(half_up_pos_test_aabb_vs_aabb_test),
+        cmocka_unit_test(inside_up_pos_test_aabb_vs_aabb_test),
         cmocka_unit_test(border_up_pos_test_aabb_vs_aabb_test),
     };
     return cmocka_run_group_tests(aabb_tests, NULL, NULL);
