a3b0092176b40604cb5dadffa6df4e2449f41039
jxv <joevargas92@gmail.com>
2015-12-03 16:43:13 -0800

    Pass same_position_test_aabb_vs_aabb

--- src/pf.c
+++ src/pf.c
@@ -1,4 +1,9 @@
 #include "pf.h"
 
 bool pf_test_aabb_vs_aabb(const pf_aabb *a, const pf_aabb *b) {
+    return
+        a->min.x < b->max.x &&
+        b->min.x < a->max.x &&
+        a->min.y < b->max.y &&
+        b->min.y < a->max.y;
 }
