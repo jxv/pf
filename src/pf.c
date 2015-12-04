9c1d7cf9aa0a69d90ac5b2c7569a7d7ffb24687a
jxv <joevargas92@gmail.com>
2015-12-04 08:52:18 -0800

    Fill in pf_test_circle_vs_circle's func body; eq_pos_test_cirle_vs_circle_test [PASSED]

--- src/pf.c
+++ src/pf.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "pf.h"
 
 bool pf_test_aabb_vs_aabb(const pf_aabb *a, const pf_aabb *b) {
@@ -9,5 +10,10 @@ bool pf_test_aabb_vs_aabb(const pf_aabb *a, const pf_aabb *b) {
 }
 
 bool pf_test_circle_vs_circle(const v2f *a_pos, const pf_circle *a, const v2f *b_pos, const pf_circle *b) {
-    return false;
+    const v2f a_center = addv2f(*a_pos, a->center);
+    const v2f b_center = addv2f(*b_pos, b->center);
+    const v2f n = subv2f(b_center, a_center);
+    float dist_sq = sqlenv2f(n);
+    float radius = a->radius + b->radius;
+    return dist_sq < radius * radius;
 }
