7e8006a190f5e4921108f5869c9703617740ee1e
jxv <joevargas92@gmail.com>
2015-12-03 16:50:03 -0800

    Change nomenclature '_success' -> '_test'

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 
 #include "pf.h"
 
-void eq_pos_test_aabb_vs_aabb_success(void **state) {
+void eq_pos_test_aabb_vs_aabb_test(void **state) {
     const pf_aabb a = {
         .min = _v2f(-1.0, -1.0),
         .max = _v2f( 1.0,  1.0),
@@ -15,7 +15,7 @@ void eq_pos_test_aabb_vs_aabb_success(void **state) {
     assert_int_equal(pf_test_aabb_vs_aabb(&a, &a), true);
 }
 
-void half_up_pos_test_aabb_vs_aabb_success(void **state) {
+void half_up_pos_test_aabb_vs_aabb_test(void **state) {
     const pf_aabb a = {
         .min = _v2f(-1.0, -1.0),
         .max = _v2f( 1.0,  1.0),
@@ -31,8 +31,8 @@ void half_up_pos_test_aabb_vs_aabb_success(void **state) {
 
 int main(void) {
     const struct CMUnitTest aabb_tests[] = {
-        cmocka_unit_test(eq_pos_test_aabb_vs_aabb_success),
-        cmocka_unit_test(half_up_pos_test_aabb_vs_aabb_success),
+        cmocka_unit_test(eq_pos_test_aabb_vs_aabb_test),
+        cmocka_unit_test(half_up_pos_test_aabb_vs_aabb_test),
     };
     return cmocka_run_group_tests(aabb_tests, NULL, NULL);
 }
