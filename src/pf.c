2881133f83f9736134bb2f10c5e3a581c4627973
jxv <joevargas92@gmail.com>
2015-12-03 17:30:43 -0800

    Fix pf_test_aabb_vs_aabb, border_right_pos_test_aabb_vs_aabb_test [PASSED]

--- src/pf.c
+++ src/pf.c
@@ -3,7 +3,7 @@
 bool pf_test_aabb_vs_aabb(const pf_aabb *a, const pf_aabb *b) {
     return
         a->min.x <= b->max.x &&
-        b->min.x < a->max.x &&
+        b->min.x <= a->max.x &&
         a->min.y <= b->max.y &&
         b->min.y <= a->max.y;
 }
