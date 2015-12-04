caa0ab8ea0b95c57f4ad0a6291f5db6a2ce559a6
jxv <joevargas92@gmail.com>
2015-12-03 17:18:32 -0800

    Pass border_left_pos_test_aabb_vs_aabb_test

--- src/pf.c
+++ src/pf.c
@@ -2,7 +2,7 @@
 
 bool pf_test_aabb_vs_aabb(const pf_aabb *a, const pf_aabb *b) {
     return
-        a->min.x < b->max.x &&
+        a->min.x <= b->max.x &&
         b->min.x < a->max.x &&
         a->min.y <= b->max.y &&
         b->min.y <= a->max.y;
