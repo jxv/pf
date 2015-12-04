616b87606020e19b8fe5670c64ad1bb384b64a6f
jxv <joevargas92@gmail.com>
2015-12-03 16:48:16 -0800

    Change nomenclature 'same' -> 'eq'; Add half_up_test_aabb_vs_aabb test

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 
 #include "pf.h"
 
-void same_position_test_aabb_vs_aabb_success(void **state) {
+void eq_pos_test_aabb_vs_aabb_success(void **state) {
     const pf_aabb a = {
         .min = _v2f(-1.0, -1.0),
         .max = _v2f( 1.0,  1.0),
@@ -15,9 +15,24 @@ void same_position_test_aabb_vs_aabb_success(void **state) {
     assert_int_equal(pf_test_aabb_vs_aabb(&a, &a), true);
 }
 
+void half_up_pos_test_aabb_vs_aabb_success(void **state) {
+    const pf_aabb a = {
+        .min = _v2f(-1.0, -1.0),
+        .max = _v2f( 1.0,  1.0),
+    };
+    
+    const pf_aabb b = {
+        .min = _v2f(-1.0, -2.0),
+        .max = _v2f( 1.0,  0.0),
+    };
+
+    assert_int_equal(pf_test_aabb_vs_aabb(&a, &b), true);
+}
+
 int main(void) {
     const struct CMUnitTest aabb_tests[] = {
-        cmocka_unit_test(same_position_test_aabb_vs_aabb_success),
+        cmocka_unit_test(eq_pos_test_aabb_vs_aabb_success),
+        cmocka_unit_test(half_up_pos_test_aabb_vs_aabb_success),
     };
     return cmocka_run_group_tests(aabb_tests, NULL, NULL);
 }
