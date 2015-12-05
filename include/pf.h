cbc2e6223a2231caa386b6b89035cc49ced948f9
jxv <joevargas92@gmail.com>
2015-12-05 11:24:39 -0800

    Note: New rule about borders for collisions; Points on tangents should not register

--- include/pf.h
+++ include/pf.h
@@ -13,6 +13,9 @@ typedef struct {
     float radius;
 } pf_circle;
 
+// Overlapping borders should not register as a collision.
+// This is because of the difficulty of finding normals between exacts points on tagnents.
+
 bool pf_test_aabb_vs_aabb(const pf_aabb *a, const pf_aabb *b);
 bool pf_test_point_vs_aabb(const v2f *a, const pf_aabb *b);
 bool pf_test_circle_vs_circle(const v2f *a_pos, const pf_circle *a, const v2f *b_pos, const pf_circle *b);
