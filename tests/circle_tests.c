6d06537d128a9d538146508d6a3836f0048237bc
jxv <joevargas92@gmail.com>
2015-12-06 15:55:35 -0800

    Conflate circle unit tests

--- tests/circle_tests.c
+++ tests/circle_tests.c
@@ -1,6 +1,6 @@
 #include "tests.h"
 
-void eq_pos_test_circle_vs_circle_test(void **state) {
+void eq_test_circle_vs_circle_test(void **state) {
 
     const v2f a_pos = _v2f(0,0);
 
@@ -12,25 +12,37 @@ void eq_pos_test_circle_vs_circle_test(void **state) {
     assert_int_equal(pf_test_circle_vs_circle(&a_pos, &a, &a_pos, &a), true);
 }
 
-void inside_up_pos_test_circle_vs_circle_test(void **state) {
+void up_test_circle_vs_circle_test(void **state) {
     const v2f a_pos = _v2f(0,0);
-
     const pf_circle a = {
         .center = _v2f(0, 0),
         .radius = 1.0,
     };
 
-    const v2f b_pos = _v2f(0,0);
-
-    const pf_circle b = {
-        .center = _v2f(0, 1),
+    const v2f inside_pos = _v2f(0, -2 + 0.1);
+    const pf_circle inside = {
+        .center = _v2f(0, 0),
         .radius = 1.0,
     };
 
-    assert_int_equal(pf_test_circle_vs_circle(&a_pos, &a, &b_pos, &b), true);
+    const v2f border_pos = _v2f(0, -2 + 0.0);
+    const pf_circle border = {
+        .center = _v2f(0, 0),
+        .radius = 1.0,
+    };
+
+    const v2f outside_pos = _v2f(0, -2 - 0.1);
+    const pf_circle outside = {
+        .center = _v2f(0, 0),
+        .radius = 1.0,
+    };
+
+    assert_int_equal(pf_test_circle_vs_circle(&a_pos, &a, &inside_pos, &inside), true);
+    assert_int_equal(pf_test_circle_vs_circle(&a_pos, &a, &border_pos, &border), true);
+    assert_int_equal(pf_test_circle_vs_circle(&a_pos, &a, &outside_pos, &outside), false);
 }
 
-void border_up_pos_test_circle_vs_circle_test(void **state) {
+void dr_test_circle_vs_circle_test(void **state) {
     const v2f a_pos = _v2f(0,0);
 
     const pf_circle a = {
@@ -38,59 +50,27 @@ void border_up_pos_test_circle_vs_circle_test(void **state) {
         .radius = 1.0,
     };
 
-    const v2f b_pos = _v2f(0,0);
-
-    const pf_circle b = {
-        .center = _v2f(0, 2),
+    const v2f border_pos = _v2f(sqrtf(2), sqrtf(2));
+    const pf_circle border = {
+        .center = _v2f(0,0),
         .radius = 1.0,
     };
 
-    assert_int_equal(pf_test_circle_vs_circle(&a_pos, &a, &b_pos, &b), true);
-}
-
-void border_dr_pos_test_circle_vs_circle_test(void **state) {
-    const v2f a_pos = _v2f(0,0);
-
-    const pf_circle a = {
-        .center = _v2f(0, 0),
+    const v2f outside_pos = _v2f(sqrtf(2) + 0.1, sqrtf(2) + 0.1);
+    const pf_circle outside = {
+        .center = _v2f(0,0),
         .radius = 1.0,
     };
 
-    const v2f b_pos = _v2f(0,0);
-
-    const pf_circle b = {
-        .center = _v2f(sqrtf(2), sqrtf(2)),
-        .radius = 1.0,
-    };
-
-    assert_int_equal(pf_test_circle_vs_circle(&a_pos, &a, &b_pos, &b), true);
-}
-
-void outside_dr_pos_test_circle_vs_circle_test(void **state) {
-    const v2f a_pos = _v2f(0,0);
-
-    const pf_circle a = {
-        .center = _v2f(0, 0),
-        .radius = 1.0,
-    };
-
-    const v2f b_pos = _v2f(0,0);
-
-    const pf_circle b = {
-        .center = _v2f(sqrtf(2) + 0.1, sqrtf(2) + 0.1),
-        .radius = 1.0,
-    };
-
-    assert_int_equal(pf_test_circle_vs_circle(&a_pos, &a, &b_pos, &b), false);
+    assert_int_equal(pf_test_circle_vs_circle(&a_pos, &a, &border_pos, &border), true);
+    assert_int_equal(pf_test_circle_vs_circle(&a_pos, &a, &outside_pos, &outside), false);
 }
 
 int run_circle_tests() {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(eq_pos_test_circle_vs_circle_test),
-        cmocka_unit_test(inside_up_pos_test_circle_vs_circle_test),
-        cmocka_unit_test(border_up_pos_test_circle_vs_circle_test),
-        cmocka_unit_test(border_dr_pos_test_circle_vs_circle_test),
-        cmocka_unit_test(outside_dr_pos_test_circle_vs_circle_test),
+        cmocka_unit_test(eq_test_circle_vs_circle_test),
+        cmocka_unit_test(up_test_circle_vs_circle_test),
+        cmocka_unit_test(dr_test_circle_vs_circle_test),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
